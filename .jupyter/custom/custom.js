4e8155e0c6f5f2231bf201e47c88bf09c9b09738
Floris <floris.defeyter@kuleuven.be>
2018-06-20 13:03:05 +0200

    Replace g^ and g$ by ^ and $ since it is unsupported

--- .jupyter/custom/custom.js
+++ .jupyter/custom/custom.js
@@ -4,8 +4,8 @@ require([
 	'nbextensions/vim_binding/vim_binding',   // depends your installation
 ], function() {
 	// Swap j/k and gj/gk (Note that <Plug> mappings)
-	CodeMirror.Vim.map("B", "g^", "normal");
-	CodeMirror.Vim.map("E", "g$", "normal");
+	CodeMirror.Vim.map("B", "^", "normal");
+	CodeMirror.Vim.map("E", "$", "normal");
 	CodeMirror.Vim.map("j", "gj", "normal");
 	CodeMirror.Vim.map("k", "gk", "normal");
 });
